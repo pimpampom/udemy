367422b83175151f54a6fce52b72c2e51c8bbf12
Yieldlove Vinh <yieldlove_vinh@Yieldloves-MacBook-Pro.local>
2019-09-09 15:05:03 +0200

    update

--- webproject/main.py
+++ webproject/main.py
@@ -1,14 +1,15 @@
 import web
 
 urls = (
-    '/(.*)', 'index'
+    '/(.*)/(.*)', 'index'
 )
 
+render = web.template.render("resources/")
 app = web.application(urls, globals())
 
 class index: 
-    def GET(self, name):
-        return '<h3>Hoi ' + name + '.</h3> What is up?'
+    def GET(self, name, age):
+        return render.main(name, age )
 
 if __name__ == "__main__":
     app.run()
